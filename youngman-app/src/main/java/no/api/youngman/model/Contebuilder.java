4fa187322e963b9016f0b7b43640fae7c86dfa14
tatummit <tatum@abctech-thailand.com>
2014-12-24 13:43:25 +0700

    Implement datamodel

---
+++ youngman-app/src/main/java/no/api/youngman/model/Contebuilder.java
@@ -0,0 +1,22 @@
+package no.api.youngman.model;
+
+public class Contebuilder {
+    String people;
+    String projectName;
+
+    public String getPeople() {
+        return people;
+    }
+
+    public void setPeople(String people) {
+        this.people = people;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+}
