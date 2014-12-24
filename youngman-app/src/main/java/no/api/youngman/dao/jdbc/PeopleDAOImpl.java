f1dc61c2d8724191dd462a0dc853bca1351ceea6
num <num@abctech-thailand.com>
2014-12-24 17:55:50 +0700

    add DAO that implement on JDBC

---
+++ youngman-app/src/main/java/no/api/youngman/dao/jdbc/PeopleDAOImpl.java
@@ -0,0 +1,34 @@
+package no.api.youngman.dao.jdbc;
+
+import no.api.youngman.dao.PeopleDAO;
+import no.api.youngman.model.People;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+
+public class PeopleDAOImpl implements PeopleDAO {
+    private static final String SQL_INSERT = "INSERT INTO collaborator (username, email, avatarurl) " +
+            "VALUES (?, ?, ?)";
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Override
+    public People insert(People model) {
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(SQL_INSERT);
+            int index = 1;
+            ps.setString(index++, model.getUsername());
+            ps.setString(index++, model.getEmail());
+            ps.setString(index, model.getAvatarUrl());
+            return ps;
+        });
+        return model;
+    }
+}
